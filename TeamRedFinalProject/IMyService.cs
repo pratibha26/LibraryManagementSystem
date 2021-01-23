3b5421093cf766b60a54f1a80042f27b3891507d
pratibha26 <pratibhajain1226@gmail.com>
2021-01-22 21:37:41 -0500

    Library system basic features

---
+++ TeamRedFinalProject/IMyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.IO;
+
+namespace TeamRedFinalProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMyService" in both code and config file together.
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract(Name = "PostSampleMethod")]
+        [WebInvoke(Method = "POST",
+         UriTemplate = "PostSampleMethod/New")]
+        string PostSampleMethod(Stream data);
+
+        [OperationContract(Name = "GetSampleMethod")]
+        [WebGet(UriTemplate = "GetSampleMethod/inputStr/{name}")]
+        string GetSampleMethod(string name);
+    }
+}
---
+++ TeamRedFinalProject/MyService.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TeamRedFinalProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    public class MyService : IMyService
+    {
+        public void DoWork()
+        {
+        }
+
+        public string PostSampleMethod(Stream data)
+        {
+            // convert Stream Data to StreamReader
+            StreamReader reader = new StreamReader(data);
+            // Read StreamReader data as string
+            string xmlString = reader.ReadToEnd();
+            string returnValue = xmlString;
+            // return the XMLString data
+            return returnValue;
+        }
+        public string GetSampleMethod(string strUserName)
+        {
+            StringBuilder strReturnValue = new StringBuilder();
+            // return username prefixed as shown below
+            strReturnValue.Append(string.Format
+            ("You have entered userName as {0}", strUserName));
+            return strReturnValue.ToString();
+        }
+    }
+}
